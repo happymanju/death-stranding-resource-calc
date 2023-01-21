34e041d16ab59682299d8ee0fb1d4379230fde7b
mpiastucki <michael.piastucki@woven-planet.global>
2023-01-21 19:44:33 +0900

    initial commit

---
+++ dtr.py
@@ -0,0 +1,79 @@
+import sys
+class Resources:
+    def __init__(self):
+        self.mats = {
+            "resins":[
+            800,
+            640,
+            480,
+            320,
+            160,
+            80,
+            40
+        ],
+            "metals":[
+            1000,
+            800,
+            600,
+            400,
+            200,
+            100,
+            50
+        ],
+            "ceramics":[
+            800,
+            640,
+            480,
+            320,
+            160,
+            80,
+            40
+        ],
+            "chemicals":[
+            600,
+            480,
+            360,
+            240,
+            120,
+            60,
+            30
+        ],
+            "alloys":[
+            1200,
+            960,
+            720,
+            480,
+            240,
+            120,
+            60
+        ]
+        }
+
+#identify correct material name in dictionary
+#loop through each amount
+#repeat: while able, subtract amount from n and increment container count
+#return container count dict
+
+def main():
+
+    remainder = int(sys.argv[1])
+    containers = {}
+    resources = Resources()
+
+    no_container = 0
+
+    for amount in resources.mats[sys.argv[2]]:
+        containers[amount] = 0
+        while remainder - amount >=0:
+            containers[amount] += 1
+            remainder -= amount
+    
+    print(remainder)
+    print(containers)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
+
