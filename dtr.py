ddbf0b95dc8903ef1f0bb813ebba5a0a7744cb2d
mpiastucki <michael.piastucki@woven-planet.global>
2023-01-23 13:02:13 +0900

    refactor: loop into function

--- dtr.py
+++ dtr.py
@@ -51,49 +51,43 @@ class Resources:
 
 
 def main():
+    containers = {}
+    resources = Resources()
+    remainder_mats = None
+    material_string = None
+
 
     if len(sys.argv) == 4:
         remainder_mats = int(sys.argv[2]) - int(sys.argv[1])
-        containers = {}
-        resources = Resources()
-
-        for amount in resources.mats[sys.argv[3]]:
-            containers[amount] = 0
-            while remainder_mats - amount >=0:
-                containers[amount] += 1
-                remainder_mats -= amount
-        
-        print("Remaining Materials: " + str(remainder_mats))
-        print(containers)
+        material_string = sys.argv[3]
     
     elif len(sys.argv) == 3:
         remainder_mats = int(sys.argv[1])
-        containers = {}
-        resources = Resources()
+        material_string = sys.argv[2]
 
-        for amount in resources.mats[sys.argv[2]]:
-            containers[amount] = 0
-            while remainder_mats - amount >=0:
-                containers[amount] += 1
-                remainder_mats -= amount
-        
-        print("Remaining Materials: " + str(remainder_mats))
-        print(containers)
     else:
         print("Error: Not enough arguments given.")
 
+    
+    def loop_resources_and_count_containers(material_string):
+        nonlocal remainder_mats
+
+        for amount in resources.mats[material_string]:
+                containers[amount] = 0
+                while remainder_mats - amount >=0:
+                    containers[amount] += 1
+                    remainder_mats -= amount
+        
+        return
+    
+    loop_resources_and_count_containers(material_string)
+    
+    print("Remaining Materials: " + str(remainder_mats))
+    print(containers)
+
     return
 
-def loop_resources_and_count_containers(material_string):
-    nonlocal containers
-    nonlocal resources
-    nonlocal remainder_mats
 
-    for amount in resources.mats[material_string]:
-            containers[amount] = 0
-            while remainder_mats - amount >=0:
-                containers[amount] += 1
-                remainder_mats -= amount
 
 if __name__ == "__main__":
     main()
